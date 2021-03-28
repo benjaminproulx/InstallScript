948f143f051404f0995ef057dc0ae644edfa2cf7
Benjamin Proulx <60246804+benjaminproulx@users.noreply.github.com>
2021-03-28 00:16:00 -0400

    Added speedtest-cli to the list

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ sudo apt install -y gnome-tweaks
 sudo apt install -y neovim
 sudo apt install -y vim
 sudo apt install -y openjdk-14-jdk
+sudo apt install -y speedtest-cli
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-get install -y apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
