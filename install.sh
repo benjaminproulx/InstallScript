34d3a5642ebaf542a19f5c37aad20d4d096e2170
Benjamin Proulx <60246804+benjaminproulx@users.noreply.github.com>
2021-03-28 12:49:48 -0400

    added python3-pip

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ sudo apt install -y neovim
 sudo apt install -y vim
 sudo apt install -y openjdk-14-jdk
 sudo apt install -y speedtest-cli
+sudo apt install -y python3-pip
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-get install -y apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
